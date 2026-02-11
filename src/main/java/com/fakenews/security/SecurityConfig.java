3ad6e97d331b35f6332d1d0505e3868da3a9c4c7
K Rahul <katarahul8@gmail.com>
2026-02-11 21:59:23 +0530

    security config updated

--- src/main/java/com/fakenews/security/SecurityConfig.java
+++ src/main/java/com/fakenews/security/SecurityConfig.java
@@ -85,7 +85,11 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://fake-news-frontend-ebon.vercel.app/"
+        ));
+
         config.setAllowedMethods(
                 List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
